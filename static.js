f341d8e5620842327640d1b6b028f01bc0af317f
ForTombstone <nefothingy@hotmail.com>
2021-01-16 07:22:31 +0000

    ok

--- static.js
+++ static.js
@@ -1,11 +1,19 @@
 const express = require("express");
-
+const serveStatic = require("serve-static")
 var app = express();
+const path = require("path")
 
-
-app.use("/", require("./static-engine")({
-	dir: __dirname,
-	name: "_site"
+function requireHTTPS(req, res, next) { 
+	/* The 'x-forwarded-proto' check is for Heroku */
+if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+	return res.redirect('https://' + req.get('host') + req.url);
+	} 
+	next();
+}
+app.use(requireHTTPS);
+app.use("/", serveStatic(path.join(__dirname, "_site"), {
+	lastModified: true,
+	extensions: ["html"]
 }))
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running!"))
